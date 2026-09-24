3a35b4480b958a92ced852514d53400400f22313
lixihuan01-rgb <li_xihuan01@student.smc.edu>
2026-09-23 17:46:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CS 52 HW2
+
+### EDIT BELOW
+
+Name: Nathan Greenfield
+
+Email: greenfield_nathan@smc.edu
+
+Platform: PC
---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 Nathan Greenfield. All rights reserved
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    /***************** Your code goes here! *****************/
+
+    // End the program nicely
+    return 0;
+}
